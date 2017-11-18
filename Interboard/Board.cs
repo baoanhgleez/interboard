f96ecb96f572444a39bfe2ad4181addc23053f9a
baoanh.gleez <anhlbse03747@fpt.edu.vn>
2017-11-18 09:03:25 +0700

    first commit

---
+++ Interboard/Board.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interboard
+{
+    public class Board
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<String> Lines { get; set; }
+
+        public Board(string name)
+        {
+            this.Name = name;
+            this.Lines = new List<string>();
+        }
+
+        public bool IsEmpty()
+        {
+            return (Lines.Count==0);
+        }
+    }
+}
